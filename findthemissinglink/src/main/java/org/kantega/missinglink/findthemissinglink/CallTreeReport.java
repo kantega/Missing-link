475fc08000befac047ca472825315ae33ce144d9
Marvin Bredal Lillehaug <marvin.lillehaug@kantega.no>
2015-07-27 20:33:39 +0200

    Keep track of the call stack, to avoid infinite loops when two methods call each other.

--- findthemissinglink/src/main/java/org/kantega/missinglink/findthemissinglink/CallTreeReport.java
+++ findthemissinglink/src/main/java/org/kantega/missinglink/findthemissinglink/CallTreeReport.java
@@ -1,6 +1,7 @@
 package org.kantega.missinglink.findthemissinglink;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -8,20 +9,24 @@ import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+import static java.util.Collections.emptySet;
+
 public abstract class CallTreeReport {
     public static List<CallNode> generateCallTree(Map<String, Set<String>> missing, Map<String, Set<String>> allReferences){
-        return getCallNodes(missing.keySet(), allReferences);
+        return getCallNodes(emptySet(), missing.keySet(), allReferences);
     }
 
-    private static List<CallNode> getCallNodes(Set<String> calls, Map<String, Set<String>> allReferences) {
+    private static List<CallNode> getCallNodes(Set<String> stack, Set<String> calls, Map<String, Set<String>> allReferences) {
         if(calls.isEmpty()){
             return Collections.emptyList();
         } else {
            return calls.stream()
                     .map(s -> {
+                        Set<String> currentStack = recordCallStack(s, stack);
                         Set<String> referencesToCall = allReferences.computeIfAbsent(s, s1 -> Collections.<String>emptySet());
-                        List<CallNode> callNodes = getCallNodes(
+                        List<CallNode> callNodes = getCallNodes(currentStack,
                                 referencesToCall.stream()
+                                        .filter(s1 -> !currentStack.contains(s1))
                                         .filter(Predicate.isEqual(s).negate()) // remove self references.
                                         .collect(Collectors.toSet()),
                                 allReferences
@@ -29,6 +34,13 @@ public abstract class CallTreeReport {
                         return new CallNode(s, callNodes);
                     }).collect(Collectors.toList());
         }
+    }
+
+
+    private static Set<String> recordCallStack(String current, Set<String> stack) {
+        Set<String> currentStack = new HashSet<>(stack);
+        currentStack.add(current);
+        return currentStack;
     }
 
     public static List<String> getCallTreeAsList(List<CallNode> callNodes){
